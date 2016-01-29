14355d580b1894d4c8288b91de9e32763da91223
Alexander Yakushev <alex@bytopia.org>
2016-01-29 21:45:41 +0200

    Version 0.1.0

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject omniconf "0.1.0-SNAPSHOT"
+(defproject omniconf "0.1.0"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
