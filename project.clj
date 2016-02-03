0f91010dd5c19b75894f5ebe8aae72717a069a6e
Alexander Yakushev <alex@bytopia.org>
2016-02-03 15:25:08 +0200

    Output test results as JUnit XML

--- project.clj
+++ project.clj
@@ -5,6 +5,8 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
 
   :profiles {:dev {:dependencies [[org.clojure/clojure "1.8.0"]]
-                   :plugins [[lein-cloverage "1.0.6"]]}
+                   :plugins [[lein-cloverage "1.0.6"]
+                             [test2junit "1.2.1"]]
+                   :test2junit-output-dir ~(or (System/getenv "CIRCLE_TEST_REPORTS") "target/test2junit")}
              :1.6 {:dependencies [[org.clojure/clojure "1.6.0"]]}
              :1.7 {:dependencies [[org.clojure/clojure "1.7.0"]]}})
