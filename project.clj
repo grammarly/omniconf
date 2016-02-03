e196e32bcdbde1ba4758d375c12a55f55605492a
Alexander Yakushev <alex@bytopia.org>
2016-02-03 12:28:02 +0200

    Test on CircleCI

--- project.clj
+++ project.clj
@@ -1,6 +1,9 @@
-(defproject com.grammarly/omniconf "0.1.0"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+(defproject com.grammarly/omniconf "0.1.1"
+  :description "Fancy configuration library for your Clojure programs"
+  :url "https://github.com/grammarly/omniconf"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.6.0"]])
+
+  :profiles {:dev {:dependencies [[org.clojure/clojure "1.8.0"]]}
+             :1.6 {:dependencies [[org.clojure/clojure "1.6.0"]]}
+             :1.7 {:dependencies [[org.clojure/clojure "1.7.0"]]}})
