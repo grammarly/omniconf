e7408c73ff2ff34509ac7d3ed05c8bd4a079fd71
Alexander Yakushev <alex@bytopia.org>
2016-09-25 18:45:48 +0300

    Bump Clojure version

--- project.clj
+++ project.clj
@@ -4,9 +4,10 @@
   :license {:name "Apache License, Version 2.0"
             :url "http://www.apache.org/licenses/LICENSE-2.0"}
 
-  :profiles {:dev {:dependencies [[org.clojure/clojure "1.8.0"]]
+  :profiles {:dev {:dependencies [[org.clojure/clojure "1.9.0-alpha12"]]
                    :plugins [[lein-cloverage "1.0.6"]
                              [test2junit "1.2.1"]]
                    :test2junit-output-dir ~(or (System/getenv "CIRCLE_TEST_REPORTS") "target/test2junit")}
              :1.6 {:dependencies [[org.clojure/clojure "1.6.0"]]}
-             :1.7 {:dependencies [[org.clojure/clojure "1.7.0"]]}})
+             :1.7 {:dependencies [[org.clojure/clojure "1.7.0"]]}
+             :1.8 {:dependencies [[org.clojure/clojure "1.8.0"]]}})
