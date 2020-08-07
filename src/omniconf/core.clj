54d07902ccb4898dd19ee91aba5282d09eab87c6
Alexander Yakushev <alex@bytopia.org>
2020-08-07 16:09:24 +0300

    Fix *data-readers* warning

--- src/omniconf/core.clj
+++ src/omniconf/core.clj
@@ -8,7 +8,7 @@
 
 (ns omniconf.core
   "Omniconf is an exhaustive configuration tool."
-  (:refer-clojure :exclude [get set])
+  (:refer-clojure :exclude [get set *data-readers*])
   (:require [clojure.core :as clj]
             [clojure.edn :as edn]
             [clojure.java.io :as io]
